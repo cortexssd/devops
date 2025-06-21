e82f58b33c6e1ac5d62eee4a4ff9e33c5956e82c
cortexssd <grandseveth@gmail.com>
2025-06-21 15:20:05 +0300

    Create ports.sh

---
+++ ports.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ $# -lt 2 ]; then
+    echo "$0 <host> <port>"
+    exit 1
+fi
+
+HOST=$1
+PORT=$2
+LOG_FILE="/var/log/port.log"
+
+nc -z -w 5 $HOST $PORT > /dev/null 2>&1
+
+if [ $? -eq 0 ]; then
+    echo "Порт $HOST:$PORT доступен"
+else
+    echo "Порт $HOST:$PORT недоступен, $LOG_FILE"
+    echo "$(date): Порт $HOST:$PORT недоступен" >> $LOG_FILE
+    exit 1
+fi
